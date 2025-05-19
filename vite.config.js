68a8f139e15d751f6163845b00aded7a5561b8d3
GleberC <gleber10@yahoo.com.br>
2025-05-18 21:41:42 -0300

    corrigido, novo deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/cagnoni_guitars_react',
+  base: '/cagnoni-guitars/', // <- esse é o nome do repositório
   plugins: [react()],
-})
-
+});
